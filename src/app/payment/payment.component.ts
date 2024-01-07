142ea0c6a1b44e1f34f27ffe5e99e3c5f64cded0
vinhnt2306 <ntvinh2306@gmail.com>
2024-01-07 21:41:26 +0700

    update

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -9,7 +9,7 @@ import { OderService } from '../service/oder.service';
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
-  styleUrls: ['./payment.component.css']
+  styleUrls: ['./payment.component.css'],
 })
 export class PaymentComponent {
   sum = 0;
@@ -18,84 +18,88 @@ export class PaymentComponent {
   //huyện
   districts: any = [];
   //xã
-  wards : any = [];
+  wards: any = [];
 
-  addRess : any[] = [];//Địa chỉ khách hàng
+  addRess: any[] = []; //Địa chỉ khách hàng
 
-  addRessChoose : any[] = [];//Địa chỉ khách hàng
+  addRessChoose: any[] = []; //Địa chỉ khách hàng
 
-  cartProductsByPayment : any[] = [];//sản phẩm trong giỏ hàng CartItem
+  cartProductsByPayment: any[] = []; //sản phẩm trong giỏ hàng CartItem
 
-  selectedProvinceId : any; // Lưu ID của tỉnh đã chọn
+  selectedProvinceId: any; // Lưu ID của tỉnh đã chọn
 
-  selectedDistrictsId : any; // Lưu ID của huyện đã chọn
+  selectedDistrictsId: any; // Lưu ID của huyện đã chọn
 
-  selectedWardId : any; // Lưu ID của xã đã chọn
+  selectedWardId: any; // Lưu ID của xã đã chọn
 
-  getListPayment : any[] = [];//khai báo listpayment
+  getListPayment: any[] = []; //khai báo listpayment
 
-  getListVouncher : any[] = [];//lấy ra list vouncher
+  getListVouncher: any[] = []; //lấy ra list vouncher
 
-  isDefaultAddress:any;//check trạng thái của địa chỉ
+  isDefaultAddress: any; //check trạng thái của địa chỉ
 
-  orderResponse:any;//dữ liệu trả về khi create order
+  orderResponse: any; //dữ liệu trả về khi create order
 
-  confirmResponse:any[] = [];//dữ liệu trả về khi confirm oder
+  confirmResponse: any[] = []; //dữ liệu trả về khi confirm oder
 
-  idPayment:any;
+  idPayment: any;
 
-  tongtien:number=0;
-  tongtienhang:number=0;
-  tienship:number=0;
+  nameUSer: any;
+  phoneNumber: any;
+
+  tongtien: number = 0;
+  tongtienhang: number = 0;
+  tienship: number = 0;
 
   constructor(
-    private addressService : AddressService,
+    private addressService: AddressService,
     private cartService: CartService,
-    private getListPaymentService : GetlistpaymentService,
-    private vouncherService : VoucherService,
-    private oderService : OderService
+    private getListPaymentService: GetlistpaymentService,
+    private vouncherService: VoucherService,
+    private oderService: OderService
   ) {}
 
   ngOnInit(): void {
     //get ra địa chỉ khách hàng
-    var addChooes:any=[]
-    var lstCart:any=[]
-    this.addressService.getAddress().subscribe((responese : any) =>{
-      this.addRessChoose = [responese.data.listAddress[0]]
+    var addChooes: any = [];
+    var lstCart: any = [];
+    this.addressService.getAddress().subscribe((responese: any) => {
+      this.addRessChoose = [responese.data.listAddress[0]];
       this.addRess = responese.data.listAddress;
-      addChooes=[responese.data.listAddress]
-      console.log(addChooes)
-    })
+      addChooes = [responese.data.listAddress];
+      console.log(addChooes);
+    });
     //get ra thành phố
     this.getProvinces();
     //get ra sản phẩm trong giỏ hàng CartItem
     this.cartService.getCartItem().subscribe((response: any) => {
-      this.cartProductsByPayment = response.data.cartItem
-      lstCart = response.data.cartItem
+      this.cartProductsByPayment = response.data.cartItem;
+      lstCart = response.data.cartItem;
       //Tính số lượng * đơn giá
-      this.sum = response.data.cartItem.reduce((next:any,prev:any)=>{return (next+prev.price*prev.quantity)},0)
-    })
+      this.sum = response.data.cartItem.reduce((next: any, prev: any) => {
+        return next + prev.price * prev.quantity;
+      }, 0);
+    });
     //gọi list payment(hình thức thanh toán)
-    this.getListPaymentService.getListPayment().subscribe((response : any) =>{
-      this.getListPayment = response.data.getLstPaymentMethod
+    this.getListPaymentService.getListPayment().subscribe((response: any) => {
+      this.getListPayment = response.data.getLstPaymentMethod;
     });
     //gọi list vouncher
-    this.vouncherService.getListVouncher().subscribe((response : any) =>{
-      this.getListVouncher = response.data.lstVoucher
+    this.vouncherService.getListVouncher().subscribe((response: any) => {
+      this.getListVouncher = response.data.lstVoucher;
     });
-    console.log(addChooes)
+    console.log(addChooes);
 
-    setTimeout(()=>{
-      if(addChooes.length>0 && lstCart.length>0){
+    setTimeout(() => {
+      if (addChooes.length > 0 && lstCart.length > 0) {
         this.ConfirmOder();
       }
-    },5000)
-
+    }, 5000);
   }
 
-  onChooseAdress(address:any):void{
-    this.addRessChoose=[address]
-    this.ConfirmOder()
+  onChooseAdress(address: any): void {
+    this.addRessChoose = [address];
+    this.ConfirmOder();
   }
 
   getProvinces(): void {
@@ -118,97 +122,105 @@ export class PaymentComponent {
     // this.getDistrictsByProvinceId(parseInt("267"));
     this.getWardByDistrictsId(parseInt(this.selectedDistrictsId.DistrictID));
   }
-  getDistrictsByProvinceId(provinceId : number) : void{
+  getDistrictsByProvinceId(provinceId: number): void {
     this.addressService.getListDistrictByProvinceId(provinceId).subscribe(
-      (data : any) => {
-        console.log(data)
+      (data: any) => {
+        console.log(data);
         this.districts = data.data;
       },
       (error) => {
         console.error('Error fetching districts:', error);
-      });
+      }
+    );
   }
-  getWardByDistrictsId(districtId : number) : void{
+  getWardByDistrictsId(districtId: number): void {
     this.addressService.getListWardByDistrictId(districtId).subscribe(
-      (data : any) => {
-
+      (data: any) => {
         this.wards = data.data;
 
-        console.log(this.wards)
+        console.log(this.wards);
       },
       (error) => {
         console.error('Error fetching districts:', error);
+      }
+    );
+  }
+
+  ConfirmOder() {
+    let cartId = this.cartProductsByPayment.map(
+      (response: any) => response.cartDetailID
+    );
+    let payload = {
+      CartDetailID: cartId,
+      PaymentMenthodID: this.getListPayment[0].id,
+      AddressDeliveryId: this.idPayment
+        ? this.idPayment
+        : this.addRessChoose[0].id,
+    };
+    this.oderService
+      .createOder(
+        payload.CartDetailID,
+        this.getListPayment[0].id,
+        payload.AddressDeliveryId
+      )
+      .subscribe((data) => {
+        this.orderResponse = data.data;
+        if (data.data) {
+          this.tongtienhang = data.data.totalAmount ? data.data.totalAmount : 0;
+          this.tienship = data.data.amountShip ? data.data.amountShip : 0;
+          this.tongtien = this.tongtienhang + this.tienship;
+        }
       });
   }
 
-  ConfirmOder(){
-    let cartId = this.cartProductsByPayment.map((response : any) =>
-        response.cartDetailID
-    )
-    let payload = {
-      CartDetailID :cartId,
-      PaymentMenthodID : this.getListPayment[0].id,
-      AddressDeliveryId : this.idPayment?this.idPayment:this.addRessChoose[0].id
-    }
-    this.oderService.createOder(payload.CartDetailID,this.getListPayment[0].id,payload.AddressDeliveryId).subscribe(data =>
-      {this.orderResponse=data.data
-        if(data.data){
-          this.tongtienhang=data.data.totalAmount?data.data.totalAmount:0;
-          this.tienship=data.data.amountShip?data.data.amountShip:0;
-          this.tongtien=this.tongtienhang+this.tienship
-        }}
-      )
-  }
-
-  onConfirmOrder(){
-    if(this.addRessChoose.length==0){
-      alert("Vui lòng thêm mới đại chỉ")
+  onConfirmOrder() {
+    if (this.addRessChoose.length == 0) {
+      alert('Vui lòng thêm mới đại chỉ');
       return;
     }
-    let cartId = this.cartProductsByPayment.map((response : any) =>
-    response.cartDetailID
-)
-    let payload= {
-      token : JSON.parse(localStorage.getItem('currentUser')??"").data.token,
-      description:"không comment",
-      cartDetailId:cartId,
+    let cartId = this.cartProductsByPayment.map(
+      (response: any) => response.cartDetailID
+    );
+    let payload = {
+      token: JSON.parse(localStorage.getItem('currentUser') ?? '').data.token,
+      description: 'không comment',
+      cartDetailId: cartId,
       totalAmountDiscount: 0,
       amountShip: this.orderResponse.amountShip,
       totalAmount: this.orderResponse.totalAmount,
       addressDelivery: this.addRessChoose[0].id,
-      paymentMethodId: this.idPayment?this.idPayment:this.addRessChoose[0].id,
+      paymentMethodId: this.idPayment
+        ? this.idPayment
+        : this.addRessChoose[0].id,
       voucherID: null,
     };
-    this.oderService.confirmOrder(payload).subscribe((res)=>{
-      this.cartProductsByPayment=[]
-      this.confirmResponse = res.data
-      console.log(this.confirmResponse)
-      alert("Đặt hàng thành công")
-
-    })
+    this.oderService.confirmOrder(payload).subscribe((res) => {
+      this.cartProductsByPayment = [];
+      this.confirmResponse = res.data;
+      console.log(this.confirmResponse);
+      alert('Đặt hàng thành công');
+    });
   }
 
-  onSubmitAddress(){
-    let payload={
-      provinceName:this.selectedProvinceId.ProvinceName,
-      provinceId:this.selectedProvinceId.ProvinceID,
-      districName:this.selectedDistrictsId.DistrictName,
-      districId:this.selectedDistrictsId.DistrictID,
-      wardName:this.selectedWardId.WardName,
-      wardId:Number(this.selectedWardId.WardCode),
-      status:this.isDefaultAddress=="true"?true:false
-    }
-    console.log("payload",payload);
-    this.addressService.createAddress(payload).subscribe((res)=>{
-      alert('Them dia chi thanh cong')
-      this.addressService.getAddress().subscribe((responese : any) =>{
-        this.addRessChoose = [responese.data.listAddress[0]]
+  onSubmitAddress() {
+    console.log(this.nameUSer);
+    return;
+    let payload = {
+      provinceName: this.selectedProvinceId.ProvinceName,
+      provinceId: this.selectedProvinceId.ProvinceID,
+      districName: this.selectedDistrictsId.DistrictName,
+      districId: this.selectedDistrictsId.DistrictID,
+      wardName: this.selectedWardId.WardName,
+      wardId: Number(this.selectedWardId.WardCode),
+      status: this.isDefaultAddress == 'true' ? true : false,
+    };
+    console.log('payload', payload);
+    this.addressService.createAddress(payload).subscribe((res) => {
+      alert('Them dia chi thanh cong');
+      this.addressService.getAddress().subscribe((responese: any) => {
+        this.addRessChoose = [responese.data.listAddress[0]];
         this.addRess = responese.data.listAddress;
-      })
-    })
+      });
+    });
   }
-
 }
-
-
-
